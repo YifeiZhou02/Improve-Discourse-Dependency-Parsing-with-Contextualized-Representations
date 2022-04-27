7bcfe774a54fef12f7809cbfda3bfb8d20392cd7
Yifei Zhou <yz639@g2-login-01.coecis.cornell.edu>
2022-04-27 10:41:08 -0400

    remove test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('./pytorch_models')
-sys.path.append('./utils')
-import argparse
-from tqdm import tqdm
-from UAS_parsing import assembled_sentence_execution, wrapper_model
-# import models
-from models import BertArcNet, BertRelationNet, RelationLSTMTagger
-# from models import BertArctNet, BertRelationNet, RelationLSTMTagger
